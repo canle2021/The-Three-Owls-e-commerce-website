da7d2407865d7ad3078672ec256b5a29a6f9a933
Yves Anglade <yanglade@gmail.com>
2022-08-03 19:47:34 -0400

    additional endpoints- not final

--- server/handlers.js
+++ server/handlers.js
@@ -1,7 +1,7 @@
 "use strict";
 
 // use this package to generate unique ids: https://www.npmjs.com/package/uuid
-// const { v4: uuidv4 } = require("uuid");
+const { v4: uuidv4 } = require("uuid");
 
 const { MongoClient } = require("mongodb");
 require("dotenv").config();
@@ -17,16 +17,13 @@ const companies = require("./data/companies.json");
 const items = require("./data/items.json");
 
 const collectionExists = async (db, collectionName) => {
-  // const collection = db.collection(collectionName);
-  // return !(collection === undefined);
-
   const collections = await db.listCollections().toArray();
   const collectionExists = collections.some((c) => c.name === collectionName);
   return collectionExists;
 };
 
 /**********************************************************/
-/*  getFlights: returns a list of all flights
+/*  getItems: returns all the items
 /**********************************************************/
 const getItems = async (req, res) => {
   const client = new MongoClient(MONGO_URI, options);
@@ -65,7 +62,7 @@ const getItems = async (req, res) => {
 };
 
 /**********************************************************/
-/*  getFlights: returns a list of all flights
+/*  getCompanies: returns all the companies
 /**********************************************************/
 const getCompanies = async (req, res) => {
   const client = new MongoClient(MONGO_URI, options);
@@ -102,6 +99,10 @@ const getCompanies = async (req, res) => {
     client.close();
   }
 };
+
+/**********************************************************/
+/*  getSingleProduct:
+/**********************************************************/
 const getSingleProduct = async (req, res) => {
   const client = new MongoClient(MONGO_URI, options);
   const dbName = "ecommerce";
@@ -136,6 +137,10 @@ const getSingleProduct = async (req, res) => {
     client.close();
   }
 };
+
+/**********************************************************/
+/*  getProductByCompany: 
+/**********************************************************/
 const getProductByCompany = async (req, res) => {
   const client = new MongoClient(MONGO_URI, options);
   const dbName = "ecommerce";
@@ -173,6 +178,10 @@ const getProductByCompany = async (req, res) => {
     client.close();
   }
 };
+
+/**********************************************************/
+/*  getSingleCategory: 
+/**********************************************************/
 const getSingleCategory = async (req, res) => {
   const client = new MongoClient(MONGO_URI, options);
   const dbName = "ecommerce";
@@ -207,6 +216,10 @@ const getSingleCategory = async (req, res) => {
     client.close();
   }
 };
+
+/**********************************************************/
+/*  getSingleCompany: 
+/**********************************************************/
 const getSingleCompany = async (req, res) => {
   const client = new MongoClient(MONGO_URI, options);
   const dbName = "ecommerce";
@@ -243,6 +256,9 @@ const getSingleCompany = async (req, res) => {
   }
 };
 
+/**********************************************************/
+/*  getCategories: 
+/**********************************************************/
 const getCategories = async (req, res) => {
   const client = new MongoClient(MONGO_URI, options);
   const dbName = "ecommerce";
@@ -284,6 +300,238 @@ const getCategories = async (req, res) => {
   }
 };
 
+/**********************************************************/
+/*  getSingleCustomer: gets a single customer
+/**********************************************************/
+const getSingleCustomer = async (req, res) => {
+  const client = new MongoClient(MONGO_URI, options);
+  const dbName = "ecommerce";
+  const {_id} = req.params;
+
+  // typeOf _id in each item in DB is a number, this step to transform req params to number for searching purpose.
+
+  try {
+    await client.connect();
+    const db = client.db(dbName);
+
+    //check if the items collection exists
+    if (!(await collectionExists(db, "customers"))) {
+      return res
+        .status(404)
+        .json({ status: 404, message: "customers collection does not exist" });
+    }
+
+    const retrieveCustomer = await db
+    .collection("customers")
+    .findOne({_id});
+
+    if (retrieveCustomer) {
+      return res.status(200).json({
+        status: 200,
+        message: `You successfully get information of customer with id: ${_id}`,
+        data: retrieveCustomer,
+      });
+    } 
+    else {
+      return res.status(404).json({
+        status: 404,
+        message: `The customer with id: ${_id} does not exist`,
+      });
+    }
+  } catch (err) {
+    res.status(500).json({ status: 500, message: err.message });
+  } finally {
+    client.close();
+  }
+};
+
+/**********************************************************/
+/*  addCustomer:  creates a new customer
+/**********************************************************/
+const addCustomer = async (req, res) => {
+const client = new MongoClient(MONGO_URI, options);
+const dbName = "ecommerce";
+const body = req.body;
+const customerFields = ["firstname", "lastname", "emal"];
+
+const newCustomer = {_id: uuidv4(), ...body};
+
+try {
+  
+  await client.connect(); 
+  const db = client.db(dbName);
+
+  //check if the fields of the new customer are the right ones
+  const bodyFields = Object.keys(body).toArray();
+  const fieldsAreRightOnes = await bodyFields.every(field => customerFields.findIndex(field.toLowerCase()) >= 0 );
+
+
+  if (!fieldsAreRightOnes) {
+    return res
+     .status(500)
+     .json({ status: 500, message: `The fields for a new customer should be ${customerFields}` });
+  }
+
+  //insert the reservation
+  const insertCustomerResult = await db.collection("customers").insertOne(newCustomer);
+
+  if (insertCustomerResult && insertCustomerResult.acknowledged) {
+      return res
+        .status(200)
+        .json({ status: 200, data: newCustomer, message: "the customer was added successfully" });
+  }
+  else {
+      res.status(500).json({status:500, message: "The customer could not be added properly"});
+  }
+}
+catch(err) {
+  console.error(`err = `, err);
+  throw err;
+}
+finally {
+  client.close();
+}
+};
+
+/**********************************************************/
+/*  updateItem: updates any or all information of an item
+/**********************************************************/
+const updateItem = async(req, res) => {
+  const client = new MongoClient(MONGO_URI, options);
+  const dbName = "ecommerce";
+  const body = req.body;
+  const {_id, ...rest } = body;
+
+  try {
+    await client.connect();
+    const db = client.db(dbName);
+
+    //check if the items collection exists
+    if (!(await collectionExists(db, "items"))) {
+      return res
+        .status(404)
+        .json({ status: 404, message: "items collection does not exist" });
+    }
+
+    //check if the _id of the product is provided
+    if (!_id) {
+      return res
+        .status(400)
+        .json({ status: 400, message: "The id of item to update is not provided"});
+    }
+
+    //check if the item can be updated
+    const updateItemResult = await db.collection("items").updateOne({_id}, {$set: {...rest}});
+
+    if (updateItemResult) {
+      if (updateItemResult.matchedCount && updateItemResult.modifiedCount) {
+        return res
+        .status(200)
+        .json({status:200, data:body, message: `The item with id: ${_id} was successfully updated.`})
+      }
+      else if (!updateItemResult.matchedCount) {
+        return res
+          .status(404)
+          .json({status:404, message: `The item with id: ${_id} does not exist`});
+      }
+      else if (!updateItemResult.modifiedCount) {
+        return res
+        .status(500)
+        .json({status:500, message: `The item with id: ${_id} could not be updated`})
+      }
+    }
+    else {
+        return res
+        .status(500)
+        .json({status:500, message: `The item with id: ${_id} could not be updated`})    
+    }
+  }
+  catch(err) {
+    console.error(err);
+    throw(err);
+  }
+  finally {
+    client.close();
+  }
+};
+
+/**********************************************************/
+/*  updateItemStock: updates the qty for an item in stock
+/**********************************************************/
+const updateItemStock = async(req, res) => {
+  const client = new MongoClient(MONGO_URI, options);
+  const dbName = "ecommerce";
+  const {_id, qty: numInStock} = req.params;
+
+  //id from params needs to be converted from string to number for update filter
+  const _idToNumber = {
+    _id: Number.parseInt(_id),
+  };
+
+  console.log(`id: ${_id} qty:${numInStock}`);
+
+  try {
+    await client.connect();
+    const db = client.db(dbName);
+
+    //check if the items collection exists
+    if (!(await collectionExists(db, "items"))) {
+      return res
+        .status(404)
+        .json({ status: 404, message: "items collection does not exist" });
+    }
+
+    //check if the _id of the item is provided
+    if (!_id) {
+      return res
+        .status(400)
+        .json({ status: 400, message: "The id of the item to update is not provided"});
+    }
+
+    //check if the _id of the qty is provided
+    if (!numInStock) {
+        return res
+            .status(400)
+            .json({ status: 400, message: "The new quantity in stock is not provided"});
+    }
+
+    //check if the item with the provided _id exists
+    const updateItemResult = await db.collection("items").updateOne(_idToNumber, {$set: {numInStock: Number.parseInt(numInStock) }});
+
+
+    if (updateItemResult) {
+      if (updateItemResult.matchedCount && updateItemResult.modifiedCount) {
+          return res
+          .status(200)
+          .json({status:200, message: `The item with id: ${_id} was successfully updated to quantity ${numInStock}.`})
+      }
+      else if (!updateItemResult.matchedCount) {
+        return res
+          .status(404)
+          .json({status:404, message: `The item with id: ${_id} does not exist`});
+      }
+      else if (!updateItemResult.modifiedCount) {
+        return res
+          .status(500)
+          .json({status:500, message: `New quantity for item with id: ${_id} could not be updated`})
+      }
+    }
+    else {
+      return res
+        .status(500)
+        .json({status:500, message: `New quantity for the item with id: ${_id} could not be updated`})    
+    }
+  }
+  catch(err) {
+    console.error(err);
+    throw(err);
+  }
+  finally {
+    client.close();
+  }
+}
+
+
 module.exports = {
   getItems,
   getCompanies,
@@ -292,4 +540,8 @@ module.exports = {
   getSingleCategory,
   getProductByCompany,
   getCategories,
+  getSingleCustomer,
+  addCustomer,
+  updateItem,
+  updateItemStock,
 };
