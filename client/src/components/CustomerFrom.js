386f442ce46ff707f43c143e1fbc6bfce0f7d1eb
Can Le <canle360@gmail.com>
2022-08-10 15:00:17 -0600

    Add customer's information form to cartPage

---
+++ client/src/components/CustomerFrom.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const CustomerForm = () => {
+  return <FormDiv>This is for the customer's information form</FormDiv>;
+};
+
+const FormDiv = styled.div``;
+
+export default CustomerForm;
