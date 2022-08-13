ef96f23e87e45c9cf7e4f0add42c8e23ed89b178
leanghengou <leanghengou5555@gmail.com>
2022-08-12 23:46:19 -0400

    product detail

--- client/src/components/QuantitySelector.js
+++ client/src/components/QuantitySelector.js
@@ -1,23 +1,28 @@
-import React, {useState, useRef, useEffet, useEffect, useContext} from "react";
+import React, {
+  useState,
+  useRef,
+  useEffet,
+  useEffect,
+  useContext,
+} from "react";
 import styled from "styled-components";
-import {CartContext} from "./CartContext";
+import { CartContext } from "./CartContext";
 
-const QuantitySelector = ({id, qty, setQty, inStock, showStock}) => {
-
-  const minValue = (!inStock)? 0: 1;
+const QuantitySelector = ({ id, qty, setQty, inStock, showStock }) => {
+  const minValue = !inStock ? 0 : 1;
   const maxValue = inStock;
   const qtyRef = useRef();
-  const {cart, getCartItemQty} = useContext(CartContext);
+  const { cart, getCartItemQty } = useContext(CartContext);
 
-  useEffect(()=> {
+  useEffect(() => {
     if (qtyRef != undefined && qtyRef.current != undefined)
       qtyRef.current.value = qty;
   }, [qty]);
 
-  const handleMinus = () =>  {
+  const handleMinus = () => {
     const currentQty = parseInt(qtyRef.current.value);
-    
-    qtyRef.current.value = (currentQty > minValue) ? currentQty - 1 : minValue;
+
+    qtyRef.current.value = currentQty > minValue ? currentQty - 1 : minValue;
 
     setQty(parseInt(qtyRef.current.value));
   };
@@ -25,74 +30,92 @@ const QuantitySelector = ({id, qty, setQty, inStock, showStock}) => {
   const handlePlus = () => {
     const currentQty = parseInt(qtyRef.current.value);
 
-    qtyRef.current.value =  (currentQty < maxValue) ? currentQty + 1 : maxValue;
-    
+    qtyRef.current.value = currentQty < maxValue ? currentQty + 1 : maxValue;
+
     setQty(parseInt(qtyRef.current.value));
   };
 
   const handleManualInput = () => {
-
     if (isNaN(-qtyRef.current.value) || parseInt(qtyRef.current.value) < 1) {
       qtyRef.current.value = 1;
-    }
-    else if (parseInt(qtyRef.current.value) > maxValue) {
+    } else if (parseInt(qtyRef.current.value) > maxValue) {
       qtyRef.current.value = maxValue;
     }
-    
+
     setQty(parseInt(qtyRef.current.value));
   };
 
-  return (
-    (inStock > 0) ?
+  return inStock > 0 ? (
     <>
-      <MinusButton disabled={qty <= 1 || !inStock} onClick={handleMinus}>-</MinusButton>
-      <Input disabled={qty === 0} ref={qtyRef} type="text" readOnly={true}/>
-      <PlusButton disabled={qty + getCartItemQty(id) >= maxValue  || !inStock} onClick={handlePlus}>+</PlusButton>
-      {showStock  && <ItemsInStock>{`In Stock(${inStock})`}</ItemsInStock>}
+      <MinusButton disabled={qty <= 1 || !inStock} onClick={handleMinus}>
+        -
+      </MinusButton>
+      <Input disabled={qty === 0} ref={qtyRef} type="text" readOnly={true} />
+      <PlusButton
+        disabled={qty + getCartItemQty(id) >= maxValue || !inStock}
+        onClick={handlePlus}
+      >
+        +
+      </PlusButton>
+      {showStock && <ItemsInStock>{`In Stock(${inStock})`}</ItemsInStock>}
     </>
-    :
+  ) : (
     <>
-      <NotAvailable>Not Available</NotAvailable>
+      <NotAvailable>Not available</NotAvailable>
     </>
-  )
+  );
 };
 
-
 const MinusButton = styled.button`
-  width: 25px;
-  height: auto;
-  border-radius: 50%;
+  width: 33px;
+  height: 33px;
+  border-radius: 5px;
   margin-right: 10px;
   display: flex;
   justify-content: center;
   align-items: center;
-  background-color: #7900d9;
+  background-color: black;
   color: white;
   font-weight: bold;
   font-size: 15px;
-
+  border: none;
   &:disabled {
     opacity: 0.4;
   }
-
+  :hover& {
+    cursor: pointer;
+    transition: 0.2s ease-in-out;
+  }
+  :active& {
+    background-color: #7900d9;
+    transition: 0s;
+  }
 `;
 
 const PlusButton = styled.button`
-  width: 25px;
-  height: auto;
-  border-radius: 50%;
+  width: 33px;
+  height: 33px;
+  border-radius: 5px;
   margin-left: 10px;
-  margin-right:10px;
+  margin-right: 10px;
   display: flex;
   justify-content: center;
   align-items: center;
-  background-color: #7900d9;
+  background-color: black;
   color: white;
   font-weight: bold;
   font-size: 15px;
-
+  border: none;
   &:disabled {
     opacity: 0.4;
+  }
+  :hover& {
+    cursor: pointer;
+    transition: 0.2s ease-in-out;
+  }
+  :active& {
+    background-color: #7900d9;
+    transition: 0s;
   }
 `;
 
@@ -102,17 +125,20 @@ const Input = styled.input`
   justify-content: center;
   align-items: center;
   text-align: center;
-  
+  width: 70px;
+  height: 30px;
+  border-radius: 5px;
+  border: 1px solid gray;
 `;
 
 const NotAvailable = styled.div`
   color: red;
-  font-weight: bold
+  font-weight: bold;
 `;
 
 const ItemsInStock = styled.div`
-    color: grey;
-    margin: 0px 10px;
+  color: grey;
+  margin: 0px 10px;
 `;
 
 export default QuantitySelector;
